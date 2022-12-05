48e989018053197e6193b6222629a2e63c2780be
sanggur591 <115924970+sanggur591@users.noreply.github.com>
2022-12-05 20:55:54 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 4.52 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -0,0 +1,48 @@
+class Solution {
+    public String solution(int[] numbers, String hand) {
+        String answer = "";
+        int lNum = 10; int rNum=12;
+        for( int i=0;i<numbers.length;i++){
+            //편의상 0->11로 해주기
+            if(numbers[i]==0) numbers[i]=11;
+            int floor = (numbers[i]-1)/3;
+            int room = (numbers[i]-1)%3;
+            //왼
+            if(room==0){
+                lNum=numbers[i];
+                answer=answer+"L";
+            }//오
+            else if(room==2){
+                rNum=numbers[i];
+                answer=answer+"R";
+            }//중간
+            else{
+                int rNumFloor = (rNum-1)/3;
+                int rNumRoom = (rNum-1)%3;
+                int lNumFloor = (lNum-1)/3;
+                int lNumRoom = (lNum-1)%3;
+                //거리가 같을때
+                if(Math.abs(rNumFloor-floor)+Math.abs(rNumRoom-room)==Math.abs(lNumFloor-floor)+Math.abs(lNumRoom-room)){
+                    if(hand.equals("left")){
+                        lNum=numbers[i];
+                        answer=answer+"L";
+                    } else if (hand.equals("right")){
+                        rNum=numbers[i];
+                        answer=answer+"R";
+                    }
+                }
+                //오가 더 가까울때
+                else if(Math.abs(rNumFloor-floor)+Math.abs(rNumRoom-room)>Math.abs(lNumFloor-floor)+Math.abs(lNumRoom-room)){
+                    lNum=numbers[i];
+                    answer=answer+"L";
+                }
+                //왼이 더 가까울때
+                else if(Math.abs(rNumFloor-floor)+Math.abs(rNumRoom-room)<Math.abs(lNumFloor-floor)+Math.abs(lNumRoom-room)){
+                    rNum=numbers[i];
+                    answer=answer+"R";
+                }
+            }
+        }
+        return answer;
+    }
+}
