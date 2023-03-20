f4e56a9f03c633ec8a99645f5ea94fdc766044e3
sanggur591 <115924970+sanggur591@users.noreply.github.com>
2023-03-20 19:49:00 +0900

    [level 2] Title: 주식가격, Time: 9.85 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        for(int i=0;i<prices.length;i++){
+            int x = 0;
+            for(int j=i+1;j<prices.length;j++) {
+                x++;
+                if (prices[i] > prices[j])
+                    break;
+            }
+            answer[i]=x;
+        }
+        return answer;
+    }
+}
